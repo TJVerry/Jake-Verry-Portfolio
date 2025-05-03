58b98c29e7deaba3e5c4d39e63a2dfd80091d4dc
TJVerry <me@jakeverry.com>
2025-05-02 20:44:10 -0400

    successfully queried  firebase from node.js

--- functions/index.js
+++ functions/index.js
@@ -7,38 +7,20 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const { setGlobalOptions } = require("firebase-functions/v2");
-const admin = require('firebase-admin');
-const cors = require('cors')({ origin: true });
+var admin = require("firebase-admin");
 
-admin.initializeApp();
+var serviceAccount = require("../serviceAccountKey.json");
 
-// exports.getProjects = onRequest({ cors: true }, async (req, res) => {
-//   try {
-//     const db = admin.firestore();
-//     const projectsCol = db.collection('products'); // Changed collection name to 'products'
-//     const snapshot = await projectsCol.orderBy('order').get();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-//     const projects = snapshot.docs.map((doc) => ({
-//       id: doc.id,
-//       ...doc.data(),
-//     }));
+const db = admin.firestore();
 
-//     // Include Firebase config in the response
-//     const firebaseConfig = {
-//       apiKey: process.env.portfolio.apikey,
-//       authDomain: process.env.portfolio.authdomain,
-//       projectId: process.env.portfolio.projectid,
-//       storageBucket: process.env.portfolio.storagebucket,
-//       messagingSenderId: process.env.portfolio.messagingsenderid,
-//       appId: process.env.portfolio.appid,
-//       measurementId: process.env.portfolio.measurementid
-//     };
+let projectRef = db.collection("projects");
 
-//     res.json({ projects: projects, firebaseConfig: firebaseConfig });
-//   } catch (error) {
-//     console.error('Error fetching projects:', error);
-//     res.status(500).send('Error fetching projects');
-//   }
-// });
+projectRef.get().then((querysnapshot) => {
+    querysnapshot.forEach((doc) => {
+        console.log(doc.data());
+    });
+});
